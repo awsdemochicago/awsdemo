7535fe56ff784807b9cb6f63ee39ad7769a0388c
Pranitha Sirimalla <pranitha.sirimalla@gmail.com>
2025-09-06 21:16:45 +0530

    added sonar cube configuration

--- src/main/java/com/example/AWS/demo/controller/GreetingController.java
+++ src/main/java/com/example/AWS/demo/controller/GreetingController.java
@@ -11,14 +11,8 @@ public class GreetingController {
 	@GetMapping
 	public String getGreetings() {
 		
-		
-<<<<<<< HEAD
-		return "Hello World";
-=======
-
 		return "Hello World";
 
->>>>>>> 408a1c789a2a65980820deed75d459316fb5d6cf
 	}
 
 }
