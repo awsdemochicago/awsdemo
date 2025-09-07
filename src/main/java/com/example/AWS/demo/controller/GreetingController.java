c2f1cfd5516b012ff363169a92c15a0cb0e6180c
awsdemochicago <awsdemochicago@gmail.com>
2025-09-07 00:19:18 +0000

    removed merge conflits and mapped to root uri

--- src/main/java/com/example/AWS/demo/controller/GreetingController.java
+++ src/main/java/com/example/AWS/demo/controller/GreetingController.java
@@ -5,20 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
 public class GreetingController {
-	
-	@GetMapping
-	public String getGreetings() {
-		
-		
-<<<<<<< HEAD
-		return "Hello World";
-=======
-
-		return "Hello World";
-
->>>>>>> 408a1c789a2a65980820deed75d459316fb5d6cf
-	}
-
+    @GetMapping
+    public String getGreetings() {
+        return "Hello World";
+    }
 }
